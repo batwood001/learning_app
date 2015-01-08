6ddd35efe8148f801eb4cfb521a3741580f78f27
Glenn Meyer <glennmeyer@gmail.com>
2015-01-08 20:39:25 +0000

    learn app foundation

---
+++ db/migrate/20150108195313_create_tables.rb
@@ -0,0 +1,40 @@
+class CreateTables < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.string :password
+      t.string :role
+    end
+
+    create_table :lectures do |t|
+      t.string :topic
+      # t.integer :user_id
+      t.string :passphrase
+      t.timestamps
+    end
+
+    create_table :questions do |t|
+      # t.integer :lecture_id
+      t.text :question
+      t.string :answer_one
+      t.string :answer_two
+      t.string :answer_three
+      t.string :answer_four
+      t.string :answer_five
+      t.string :answer_six
+      t.string :answer_seven
+      t.string :answer_eight
+      t.string :correct_answer
+      t.timestamps
+    end
+
+    create_table :responses do |t|
+      # t.integer :user_id
+      # t.integer :question_id
+      t.string :response
+      t.timestamps
+    end
+  end
+end
