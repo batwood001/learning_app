cb2afbfaa77ab21e7e0f1e39f0b69af3779e4cd9
Glenn Meyer <glennmeyer@gmail.com>
2015-01-09 23:35:43 +0000

    endpoint tweaking

--- lib/entities/response.rb
+++ lib/entities/response.rb
@@ -2,5 +2,6 @@ module LearningApp
   class Response < ActiveRecord::Base
     belongs_to :user
     belongs_to :question
+    belongs_to :lectures
   end
 end
--- server.rb
+++ server.rb
@@ -3,3 +3,121 @@ require 'sinatra/reloader'
 require 'sinatra/flash'
 
 require_relative 'config/environments.rb'
+
+# For le Mac users
+set :bind, '0.0.0.0'
+enable :sessions
+
+get '/' do
+  if session[:user_id]
+
+  end
+end
+
+# /lectures/#/questions/#
+
+post '/signup' do
+
+  user_info = LearningApp::UserRepo.create({
+    'first_name' => params['first_name'],
+    'last_name' => params['last_name'],
+    'email' => params['email'],
+    'password' => params['password'],
+    'role' => params['role']
+    })
+
+  if user_info.errors
+    flash[:error] = user_info.errors.messages
+    redirect to('/')
+  else
+    session = {
+      user_id: user_info['id'],
+      first_name: user_info['first_name'],
+      last_name: user_info['last_name'],
+      email: user_info['email'],
+      role: user_info['role']
+    }
+    redirect to('/lectures')
+  end
+end
+
+post '/signin' do
+  user_info = LearningApp::UserRepo.validate_by_email_and_password(params['email'],
+    params['password'])
+
+  if user_info.errors
+    flash[:error] = user_info.errors.messages
+    redirect to('/')
+  else
+    session = {
+      user_id: user_info['id'],
+      first_name: user_info['first_name'],
+      last_name: user_info['last_name'],
+      email: user_info['email'],
+      role: user_info['role']
+    }
+    redirect to('/lectures')
+  end
+  
+end
+
+get '/lectures' do
+  LearningApp::LectureRepo.get_all_presented_lectures_questions_and_responses
+end
+
+post '/lectures' do
+  lecture_id = LearningApp::LectureRepo.create_by_user_id_and_topic(session[:user_id], params['topic']) if session[:role] == 'instructor'
+
+  redirect to('/lectures/' + lecture_id)
+end
+
+get '/lectures/:id' do
+  if session[:role] == 'student'
+    LearningApp::QuestionRepo.get_active_question
+  else
+    LearningApp::QuestionRepo.get_correct_answer_by_lecture_id(session[:lecture_id])
+  end
+end
+
+post '/lectures/:id/activate' do
+  LearningApp::LectureRepo.set_active_by_lecture_id(params['lecture_id'])
+end
+
+post '/lectures/:id/deactivate' do
+  LearningApp::LectureRepo.set_inactive_by_lecture_id(params['lecture_id'])
+end
+
+post '/lectures/:id/questions' do
+  question_info = {
+    'lecture_id' => session[:lecture_id],
+    'question' => params['question'],
+    'answer_one' => params['answer_one'],
+    'answer_two' => params['answer_two'],
+    'answer_three' => params['answer_three'],
+    'answer_four' => params['answer_four'],
+    'answer_five' => params['answer_five'],
+    'answer_six' => params['answer_six'],
+    'answer_seven' => params['answer_seven'],
+    'answer_eight' => params['answer_eight'],
+    'correct_answer' => params['correct_answer'],
+  }
+  LearningApp::QuestionRepo.create_by_lecture_id(question_info)
+
+  redirect to('lectures/:id')
+end
+
+get '/lectures/:lecture_id/questions/:id' do
+  LearningApp::QuestionRepo.get_question_and_answer_by_question_id(params['question_id'])
+end
+
+post '/lectures/:lecture_id/questions/:id/activate' do
+  LearningApp::QuestionRepo.set_active_by_question_id(params['question_id'])
+end
+
+post '/lectures/:lecture_id/questions/:id/deactivate' do
+  LearningApp::QuestionRepo.set_inactive_by_question_id(params['question_id'])
+end
+
+post '/lectures/:lecture_id/questions/:question_id/response' do
+  LearningApp::ResponseRepo.create_response_by_user_id_and_question_id(session[:user_id], params['question_id'], params['response'])
+end
