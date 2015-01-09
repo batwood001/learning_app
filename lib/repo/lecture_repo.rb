6bf41c9087a7fa50592f87e58f66ca94f0f0f2b8
Glenn Meyer <glennmeyer@gmail.com>
2015-01-09 17:00:04 +0000

    done with user_repo, for reals this time

--- lib/repo/lecture_repo.rb
+++ lib/repo/lecture_repo.rb
@@ -1,5 +1,8 @@
 module LearningApp
   class LectureRepo
+    def self.create_by_user_id(user_id)
+    end
+
     def self.get_lecture_id_and_topic_by_user_id(user_id)
       Lecture.find_by(user_id)
     end
@@ -8,9 +11,6 @@ module LearningApp
     end
 
     def self.set_inactive_by_lecture_id(lecture_id)
-    end
-
-    def self.create_by_user_id(user_id)
     end
   end
 end
