bcfda7370779d4bf1af10d88cb816ced7c61bc02
Nora Neyland <noraleeneyland@gmail.com>
2015-01-10 14:44:13 -0600

    testing out different signup submit

--- public/stylesheets/bootstrap/js/signup.js
+++ public/stylesheets/bootstrap/js/signup.js
@@ -1,6 +1,7 @@
 function ajaxSignup () {
  
-  $('#sign-up-modal-button').click(function() {
+  $('#form.signUpForm').on('submit', function(e) {
+    e.preventDefault();
     $(".authContainer").hide();
 
     console.log("in signup");
@@ -27,37 +28,37 @@ function ajaxSignup () {
   })
 };
 
-function enterSignup () {
+// function enterSignup () {
   
-  $('#signUpForm').keypress(function(e) {
-    if ((e.keyCode == 13) && (e.target.type != "textarea")) {
-      e.preventDefault();
-      $(".authContainer").hide();
+//   $('#signUpForm').keypress(function(e) {
+//     if ((e.keyCode == 13) && (e.target.type != "textarea")) {
+//       e.preventDefault();
+//       $(".authContainer").hide();
 
-      console.log("in signup");
-      console.log("first_name: " + $('[data-id="first_name"]').val());
-      console.log("email: " + $('[data-id="email"]').val());
-      console.log("password: " + $('[data-id="password"]').val());
-      $.post(
-        "/signup",
-        {
-          first_name: $('[data-id="first_name"]').val(), 
-          last_name: $('[data-id="last_name"]').val(), 
-          password: $('[data-id="signup-password"]').val(),
-          email: $('[data-id="signup-email"]').val(),
-          role: 'student' // Temporary until we come up with a solution to identify instructors
-        }
-      ).success(function () {
-        console.log("signup success")
-        // inform user of successful signup, hide and clear inputs.
-        console.log($('[data-id="first_name"]').val() + " successfully signed up.");
-        $(".authContainer").hide();
-        $("#signUpModal").hide();
-        $("#logoutBtn").show();
-      });
-    }
-  });
-};
+//       console.log("in signup");
+//       console.log("first_name: " + $('[data-id="first_name"]').val());
+//       console.log("email: " + $('[data-id="email"]').val());
+//       console.log("password: " + $('[data-id="password"]').val());
+//       $.post(
+//         "/signup",
+//         {
+//           first_name: $('[data-id="first_name"]').val(), 
+//           last_name: $('[data-id="last_name"]').val(), 
+//           password: $('[data-id="signup-password"]').val(),
+//           email: $('[data-id="signup-email"]').val(),
+//           role: 'student' // Temporary until we come up with a solution to identify instructors
+//         }
+//       ).success(function () {
+//         console.log("signup success")
+//         // inform user of successful signup, hide and clear inputs.
+//         console.log($('[data-id="first_name"]').val() + " successfully signed up.");
+//         $(".authContainer").hide();
+//         $("#signUpModal").hide();
+//         $("#logoutBtn").show();
+//       });
+//     }
+//   });
+// };
 
 ajaxSignup();
-enterSignup();
+// enterSignup();
