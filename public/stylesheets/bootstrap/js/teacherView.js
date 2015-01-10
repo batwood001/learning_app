b50cb68dcd9f97de7a4ccb72c8e6c8baff58baf3
Michael Bashour <michael.s.bashour@gmail.com>
2015-01-09 18:04:57 -0600

    nav works.

--- public/stylesheets/bootstrap/js/teacherView.js
+++ public/stylesheets/bootstrap/js/teacherView.js
@@ -1,9 +1,22 @@
 function teacherView () {
   $("#toggleTeacherView").click(function() {
     $(".addQuestion").show();
+    $(".student-overview").hide();
     $(".toggleStudentView").show();
     $(".toggleTeacherView").hide();
+    $(".activeQuestions").hide();
+	$(".studentOverviewLink").hide();
+    $(".teacherOverviewLink").show();
   })
 };
 
 teacherView();
+
+function getTeacherOverview () {
+  $(".teacherOverviewLink").click(function() {
+  	$(".student-overview").show();
+  	$(".activeQuestions").hide();
+  })
+}
+
+getTeacherOverview();
