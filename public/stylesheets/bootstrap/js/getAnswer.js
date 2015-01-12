1c5bbc36beac1d7cd0c002c994c679b90d4449af
Nora Neyland <noraleeneyland@gmail.com>
2015-01-11 18:02:03 -0600

    trying to get answer page

---
+++ public/stylesheets/bootstrap/js/getAnswer.js
@@ -0,0 +1,22 @@
+function getAnswer () {
+ $('.quizAnswer').click(function(e) {
+  e.preventDefault()
+    $(".student-overview").hide();
+    $(".addQuestion").hide();
+    $(".studentOverviewLink").show();
+    $(".teacherOverviewLink").hide();
+    $(".activeQuestions").show();
+    $.get("/lectures/" + $(this).find('a').attr('data-id'), function (data) {
+      console.log("the get request worked");
+      var parsed = jQuery.parseJSON( data );
+      console.log(parsed);
+      $(".question-text").html(parsed['question']);
+      $(".answer-options").children().remove();
+      for (var element in numbers) {
+        if (parsed['answer_' + numbers[element]]) {
+          $(".answer-options").append("<a href='#' class='answer-" + numbers[element] + " list-group-item' data-id='" + parsed['answer_' + numbers[element]] + "'>" + parsed['answer_' + numbers[element]] + "</a>")
+        }
+      }
+    })
+  })
+}
